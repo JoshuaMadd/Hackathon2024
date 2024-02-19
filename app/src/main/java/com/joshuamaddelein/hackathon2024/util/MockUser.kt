c2bcec64f251f958d56bdd5345972b36347dba44
JoshuaMadd <joshua.maddelein@student.vives.be>
2024-02-19 16:51:01 +0100

    MockUser

---
+++ app/src/main/java/com/joshuamaddelein/hackathon2024/util/MockUser.kt
@@ -0,0 +1,19 @@
+package com.joshuamaddelein.hackathon2024.util
+
+import com.joshuamaddelein.hackathon2024.data.model.Les
+import com.joshuamaddelein.hackathon2024.data.model.User
+import java.time.LocalDate
+import java.time.LocalTime
+
+object MockUser {
+    private val les1 = Les("Practice Enterprise","H3.08", LocalDate.now().plusDays(0), LocalTime.of(8,45),LocalTime.of(17,45))
+    private val les2 = Les("Advanced Networks","H4.17", LocalDate.now().plusDays(1), LocalTime.of(8,45),LocalTime.of(10,45))
+    private val les3 = Les("Network Automation 1","H2.11", LocalDate.now().plusDays(1), LocalTime.of(11,0),LocalTime.of(13,0))
+    private val les4 = Les("Practice Enterprise","H60.04", LocalDate.now().plusDays(1), LocalTime.of(14,0),LocalTime.of(17,15))
+    private val les5 = Les("Cybersecurity","H60.07", LocalDate.now().plusDays(2), LocalTime.of(14,0),LocalTime.of(16,15))
+    private val lessen = listOf<Les>(les1, les2, les3, les4, les5);
+    private val user = User(lessen,"Joshua","Maddelein","r0879578");
+    fun getUser() : User {
+        return user;
+    }
+}
