409f0faf467de8fd475af8fe7041a18e147be69b
jelle.gemin@student.vives.be <jelle.gemin@student.vives.be>
2024-02-20 10:21:35 +0100

    Responsive done

--- app/src/main/java/com/joshuamaddelein/hackathon2024/MainActivity.kt
+++ app/src/main/java/com/joshuamaddelein/hackathon2024/MainActivity.kt
@@ -4,22 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.glance.GlanceModifier
-import androidx.glance.appwidget.lazy.LazyColumn
-import androidx.glance.appwidget.lazy.items
-import androidx.glance.layout.padding
-import com.joshuamaddelein.hackathon2024.data.model.Les
 import com.joshuamaddelein.hackathon2024.ui.theme.Hackathon2024Theme
-import com.joshuamaddelein.hackathon2024.util.MockUser
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ItemList(MockUser.getLessen())
+                    Greeting("Android")
                 }
             }
         }
@@ -39,30 +30,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ItemList(list: List<Les>)
-{
-    LazyColumn(
-        modifier = GlanceModifier.padding(
-            start = 20.dp,
-            end = 20.dp,
-            bottom = 60.dp,
-            top = 20.dp
-        )
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
     )
-    {
-        items(list) { item ->
-            ItemCard(item)
-        }
-    }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun ItemCard(les : Les)
-{
-    Card(modifier = Modifier.padding(top = 10.dp))
-    {
-        les.naam
-        les.datum
-        les.lokaal
+fun GreetingPreview() {
+    Hackathon2024Theme {
+        Greeting("Android")
     }
 }
--- app/src/main/java/com/joshuamaddelein/hackathon2024/ui/SimpleWidget.kt
+++ app/src/main/java/com/joshuamaddelein/hackathon2024/ui/SimpleWidget.kt
@@ -4,62 +4,99 @@ import android.content.Context
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.LocalSize
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.background
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
-import androidx.glance.color.ColorProvider
-import androidx.glance.layout.*
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
 import androidx.glance.text.Text
-import androidx.glance.text.TextStyle
 import com.joshuamaddelein.hackathon2024.data.model.Les
-import com.joshuamaddelein.hackathon2024.util.MockUser
 
-class SimpleWidget: GlanceAppWidget() {
+class SimpleWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent { ContentView() }
     }
 
+    companion object {
+        private val SMALL_RECTANGLE = DpSize(50.dp, 100.dp)
+        private val MEDIUM_RECTANGLE = DpSize(50.dp, 200.dp)
+        private val LARGE_RECTANGLE = DpSize(50.dp, 300.dp)
+    }
+
+    override val sizeMode = SizeMode.Responsive(
+        setOf(
+            SMALL_RECTANGLE,
+            MEDIUM_RECTANGLE,
+            LARGE_RECTANGLE
+        )
+    )
+
     @Composable
-    private fun ContentView()
-    {
-        Column(modifier = GlanceModifier
-            .fillMaxSize()
-            .background(day= Color.Black, night=Color.White),
+    private fun ContentView() {
+        val size = LocalSize.current
+
+        Column(
+            modifier = GlanceModifier
+                .fillMaxSize()
+                .background(day = Color.Black, night = Color.White),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            ItemList(list = MockUser.getUser().lessen)
-            Spacer(modifier = GlanceModifier.height(10.dp))
-            Text(text = "VIVES PLUS WIDGET test",
-                style = TextStyle(color = ColorProvider(day=Color.White, night=Color.Black),
-                    fontSize = 16.sp)
-            )
+            //ItemList(list = MockUser.getUser().lessen)
+//            Spacer(modifier = GlanceModifier.height(10.dp))
+//            Text(text = "VIVES PLUS WIDGET test",
+//                style = TextStyle(color = ColorProvider(day=Color.White, night=Color.Black),
+//                    fontSize = 16.sp)
+//            )
+
+            if (size.height >= LARGE_RECTANGLE.height) {
+                Large_Widget()
+            } else if (size.height > SMALL_RECTANGLE.height && size.height <= MEDIUM_RECTANGLE.height) {
+                Medium_Widget()
+            } else {
+                Small_Widget()
+            }
         }
     }
 
     @Composable
-    fun ItemList(list: List<Les>)
-    {
+    fun Large_Widget() {
+        Text(text = "Big widget")
+    }
+
+    @Composable
+    fun Medium_Widget() {
+        Text(text = "Medium widget")
+    }
+
+    @Composable
+    fun Small_Widget() {
+        Text(text = "Small widget")
+    }
+
+    @Composable
+    fun ItemList(list: List<Les>) {
         LazyColumn()
         {
             items(list)
-            {
-                les ->
+            { les ->
                 ItemCard(les)
             }
         }
     }
 
     @Composable
-    fun ItemCard(les :Les)
-    {
+    fun ItemCard(les: Les) {
         Card()
         {
             les.naam
