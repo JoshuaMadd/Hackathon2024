d5767c1c58e3c29f41450725728a1a8816c7416f
SamVandenBerghe10 <sam.vandenberghe@student.vives.be>
2024-02-20 10:36:23 +0100

    lazycolumn widget

--- app/src/main/java/com/joshuamaddelein/hackathon2024/MainActivity.kt
+++ app/src/main/java/com/joshuamaddelein/hackathon2024/MainActivity.kt
@@ -3,14 +3,27 @@ package com.joshuamaddelein.hackathon2024
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceModifier
@@ -31,7 +44,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ItemList(MockUser.getLessen())
+                    ItemList(MockUser.getUser().lessen)
                 }
             }
         }
@@ -39,10 +52,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun ItemCard(les: Les)
+{
+    Card(modifier = Modifier.padding(top = 20.dp)) {
+        Column {
+            Text(text = les.naam)
+            Text(text = les.lokaal)
+        }
+    }
+}
+
+@Composable
 fun ItemList(list: List<Les>)
 {
-    LazyColumn(
-        modifier = GlanceModifier.padding(
+    androidx.compose.foundation.lazy.LazyColumn(
+        modifier = Modifier.padding(
             start = 20.dp,
             end = 20.dp,
             bottom = 60.dp,
@@ -53,16 +77,5 @@ fun ItemList(list: List<Les>)
         items(list) { item ->
             ItemCard(item)
         }
-    }
-}
-
-@Composable
-fun ItemCard(les : Les)
-{
-    Card(modifier = Modifier.padding(top = 10.dp))
-    {
-        les.naam
-        les.datum
-        les.lokaal
     }
 }
--- app/src/main/java/com/joshuamaddelein/hackathon2024/ui/SimpleWidget.kt
+++ app/src/main/java/com/joshuamaddelein/hackathon2024/ui/SimpleWidget.kt
@@ -1,15 +1,24 @@
 package com.joshuamaddelein.hackathon2024.ui
 
+import android.app.AlertDialog
 import android.content.Context
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.background
+import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.lazy.LazyColumn
 import androidx.glance.appwidget.lazy.items
 import androidx.glance.appwidget.provideContent
@@ -29,42 +38,52 @@ class SimpleWidget: GlanceAppWidget() {
     @Composable
     private fun ContentView()
     {
+
         Column(modifier = GlanceModifier
             .fillMaxSize()
-            .background(day= Color.Black, night=Color.White),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalAlignment = Alignment.CenterVertically
+            .background(day= Color.White, night=Color.White),
+            horizontalAlignment = Alignment.Start,
+            verticalAlignment = Alignment.Top
         ) {
-            ItemList(list = MockUser.getUser().lessen)
-            Spacer(modifier = GlanceModifier.height(10.dp))
             Text(text = "VIVES PLUS WIDGET test",
-                style = TextStyle(color = ColorProvider(day=Color.White, night=Color.Black),
+                style = TextStyle(color = ColorProvider(day=Color.Black, night=Color.Black),
                     fontSize = 16.sp)
             )
+            ItemList(list = MockUser.getUser().lessen)
         }
+    }
+
+    @Composable
+    fun ItemCard(les: Les)
+    {
+        Column(modifier = GlanceModifier.padding(bottom = 10.dp)) {
+            Column(modifier = GlanceModifier.background(Color.Red, Color.Red).height(60.dp).fillMaxWidth().cornerRadius(10.dp)) {
+                Text(text = les.naam)
+                Text(text = les.lokaal)
+                Text(text = les.datum.toString())
+            }
+        }
+
+
     }
 
     @Composable
     fun ItemList(list: List<Les>)
     {
-        LazyColumn()
+        LazyColumn(
+            modifier = GlanceModifier.padding(top = 5.dp, bottom = 5.dp).height(200.dp).padding(
+                start = 20.dp,
+                end = 20.dp,
+                bottom = 20.dp,
+                top = 20.dp
+            )
+        )
         {
-            items(list)
-            {
-                les ->
-                ItemCard(les)
+            items(list) { item ->
+//                Text(text = item.naam, style = TextStyle(color = androidx.glance.unit.ColorProvider(Color.White)))
+                ItemCard(les = item)
+                Spacer(modifier = GlanceModifier.height(10.dp))
             }
-        }
-    }
-
-    @Composable
-    fun ItemCard(les :Les)
-    {
-        Card()
-        {
-            les.naam
-            les.datum
-            les.lokaal
         }
     }
 }
